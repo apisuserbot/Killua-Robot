1a62f1636a709060d2870ec6e32644873f038415
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2022-01-29 19:31:58 +0700

    Fixed to project

--- KilluaRobot/events.py
+++ KilluaRobot/events.py
@@ -146,7 +146,7 @@ def bot(**args):
     return decorator
 
 
-def SkyzuRobot(**args):
+def KilluaRobot(**args):
     pattern = args.get("pattern", None)
     disable_edited = args.get("disable_edited", False)
     ignore_unsafe = args.get("ignore_unsafe", False)
