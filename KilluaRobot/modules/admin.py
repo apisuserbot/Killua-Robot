50ffd99034b5522e567c8474c090db114bd04565
Fariz <84852749+fjgaming212@users.noreply.github.com>
2022-01-30 07:58:06 +0700

    fix forgot

--- KilluaRobot/modules/admin.py
+++ KilluaRobot/modules/admin.py
@@ -5,9 +5,9 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from SkyzuRobot import DRAGONS, dispatcher
-from SkyzuRobot.modules.disable import DisableAbleCommandHandler
-from SkyzuRobot.modules.helper_funcs.chat_status import (
+from KilluaRobot import DRAGONS, dispatcher
+from KilluaRobot.modules.disable import DisableAbleCommandHandler
+from KilluaRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     can_pin,
     can_promote,
@@ -16,13 +16,13 @@ from SkyzuRobot.modules.helper_funcs.chat_status import (
     ADMIN_CACHE,
 )
 
-from SkyzuRobot.modules.helper_funcs.admin_rights import user_can_changeinfo, user_can_promote
-from SkyzuRobot.modules.helper_funcs.extraction import (
+from KilluaRobot.modules.helper_funcs.admin_rights import user_can_changeinfo, user_can_promote
+from KilluaRobot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from SkyzuRobot.modules.log_channel import loggable
-from SkyzuRobot.modules.helper_funcs.alternate import send_message
+from KilluaRobot.modules.log_channel import loggable
+from KilluaRobot.modules.helper_funcs.alternate import send_message
 
 
 @bot_admin
