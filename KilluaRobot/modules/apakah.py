d567262dd0c871c032df38e1f391f5b5d1ff64ab
apisuserbot <80087526+apisuserbot@users.noreply.github.com>
2022-01-31 15:03:55 +0700

    Add file to project

---
+++ KilluaRobot/modules/apakah.py
@@ -0,0 +1,26 @@
+import random
+from KilluaRobot.events import register
+from KilluaRobot import telethn
+
+APAKAH_STRING = [
+    "Iya",
+    "Tidak",
+    "Mungkin",
+    "Mungkin Tidak",
+    "Bisa jadi",
+    "Mungkin Tidak",
+    "Tidak Mungkin",
+    "YNTKTS",
+    "Pala bapak kau pecah",
+    "Apa iya?",
+    "Tanya aja sama mamak kau tu pler",
+]
+
+
+@register(pattern="^/apakah ?(.*)")
+async def apakah(event):
+    quew = event.pattern_match.group(1)
+    if not quew:
+        await event.reply("Berikan saya pertanyaan 😼")
+        return
+    await event.reply(random.choice(APAKAH_STRING))
