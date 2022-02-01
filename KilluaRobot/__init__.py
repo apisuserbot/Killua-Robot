f9dc665a133098f189b6328c8f55f3faada34cc5
Unknown <90582750+unknownkz@users.noreply.github.com>
2022-02-01 09:17:59 +0700

    Update __init__.py

--- KilluaRobot/__init__.py
+++ KilluaRobot/__init__.py
@@ -26,7 +26,7 @@ logging.basicConfig(
     handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
     level=logging.INFO,
 )
-LOGGER = getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
 # if version < 3.6, stop bot.
 if sys.version_info[0] < 3 or sys.version_info[1] < 6:
