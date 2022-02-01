0c89507bfd6d1cb5f2572b154d132cac6741ec00
Unknown <90582750+unknownkz@users.noreply.github.com>
2022-02-01 19:33:56 +0700

    Update mongo.py

--- KilluaRobot/mongo.py
+++ KilluaRobot/mongo.py
@@ -4,7 +4,7 @@ from motor import motor_asyncio
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 from KilluaRobot import MONGO_DB_URI
-from KilluaRobot.config import get_int_key, get_str_key
+from KilluaRobot.configure import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
