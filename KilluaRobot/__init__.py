2a8d3d625f07f45ad9b5dbd37d7d47a4c0258b8e
Unknown <90582750+unknownkz@users.noreply.github.com>
2022-02-02 18:42:27 +0700

    Update __init__.py

--- KilluaRobot/__init__.py
+++ KilluaRobot/__init__.py
@@ -28,10 +28,10 @@ logging.basicConfig(
 )
 LOGGER = logging.getLogger(__name__)
 
-# if version < 3.6, stop bot.
-if sys.version_info[0] < 3 or sys.version_info[1] < 6:
+# if version < 3.9, stop bot.
+if sys.version_info[0] < 3 or sys.version_info[1] < 9:
     LOGGER.error(
-        "You MUST have a python version of at least 3.6! Multiple features depend on this Bot quitting"
+        "You MUST have a python version of at least 3.9! Multiple features depend on this Bot quitting"
     )
     sys.exit(1)
 
