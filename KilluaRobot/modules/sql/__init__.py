24a2d6d41b9bdad4f73eb3c7fea4ce85b22172bb
Unknown <90582750+unknownkz@users.noreply.github.com>
2022-02-03 10:55:15 +0700

    Update __init__.py

--- KilluaRobot/modules/sql/__init__.py
+++ KilluaRobot/modules/sql/__init__.py
@@ -1,5 +1,5 @@
 import sys
-from my_web_framework import get_current_request, on_request_end
+from my_web_framework import get_current_request
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
