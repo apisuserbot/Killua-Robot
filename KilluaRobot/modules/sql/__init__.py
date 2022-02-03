2fcfb82ae835f54b30d685449821fe4d4d29b330
Unknown <90582750+unknownkz@users.noreply.github.com>
2022-02-03 10:53:04 +0700

    Update __init__.py

--- KilluaRobot/modules/sql/__init__.py
+++ KilluaRobot/modules/sql/__init__.py
@@ -1,5 +1,5 @@
 import sys
-import datetime
+from my_web_framework import get_current_request, on_request_end
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
@@ -10,13 +10,13 @@ if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 def start() -> scoped_session:
-    engine = create_engine(DATABASE_URL)
+    engine = create_engine(DATABASE_URL, echo=True)
     Unknown.info("[PostgreSQL] Connecting to database...")
+    BASE = declarative_base()
     BASE.metadata.bind = engine
-    BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+    BASE.metadata.create_all(engine, checkfirst=True)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False), scopefunc=get_current_request)
 
-BASE = declarative_base()
 try:
     SESSION = start()
 except Exception as e:
