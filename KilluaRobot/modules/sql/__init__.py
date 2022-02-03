19dc0118847dfd4bd200c5e81401ef7f4c2189c4
Unknown <90582750+unknownkz@users.noreply.github.com>
2022-02-03 07:57:08 +0700

    Update __init__.py

--- KilluaRobot/modules/sql/__init__.py
+++ KilluaRobot/modules/sql/__init__.py
@@ -3,13 +3,13 @@ import datetime
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
-from KilluaRobot import DB_URI
+from KilluaRobot import DATABASE_URL
 
 BASE = declarative_base()
 
 
 def start() -> scoped_session:
-    engine = create_engine(DB_URI)
+    engine = create_engine(DATABASE_URL)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
