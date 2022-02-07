2e9468f631006f98644fc1ba7e9d83e12bbd04ba
Unknown <90582750+unknownkz@users.noreply.github.com>
2022-02-07 23:57:01 +0700

    Update __init__.py

--- KilluaRobot/modules/sql/__init__.py
+++ KilluaRobot/modules/sql/__init__.py
@@ -1,8 +1,7 @@
-import sys
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
-from KilluaRobot import DB_URI, LOGGER as Unknown
+from KilluaRobot import DB_URI
 
 
 BASE = declarative_base()
@@ -10,15 +9,9 @@ BASE = declarative_base()
 
 def start() -> scoped_session:
     engine = create_engine(DB_URI, echo=True, client_encoding="utf8")
-    Unknown.info("[PostgreSQL] Connecting to database...")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine, checkfirst=True)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
-try:
-    SESSION = start()
-except Exception as e:
-    Unknown.exception(f'[PostgreSQL] Failed to connect due to {e}')
-    exit()
-   
-Unknown.info("[PostgreSQL] Connection successful, session started.")
+
+SESSION = start()
