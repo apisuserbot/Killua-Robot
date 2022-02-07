2ddbf620073da99ff10c49d4321eae777a2e086d
Unknown <90582750+unknownkz@users.noreply.github.com>
2022-02-08 01:48:34 +0700

    Update notes_sql.py

--- KilluaRobot/modules/sql/notes_sql.py
+++ KilluaRobot/modules/sql/notes_sql.py
@@ -1,8 +1,10 @@
-# Note: chat_id's are stored as strings because the int is too large to be stored in a PSQL database.
-import threading
+try:
+   from KilluaRobot.modules.helper_funcs.msg_types import Types
+   from KilluaRobot.modules.sql import BASE, SESSION
+except ImportError:
+    raise AttributeError
 
-from KilluaRobot.modules.helper_funcs.msg_types import Types
-from KilluaRobot.modules.sql import BASE, SESSION
+import threading
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText, distinct, func
 
 
