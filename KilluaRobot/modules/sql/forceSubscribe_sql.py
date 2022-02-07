29f8a83057b746f8cd842f2e8ef19905f50e5718
Unknown <90582750+unknownkz@users.noreply.github.com>
2022-02-08 01:45:24 +0700

    Update forceSubscribe_sql.py

--- KilluaRobot/modules/sql/forceSubscribe_sql.py
+++ KilluaRobot/modules/sql/forceSubscribe_sql.py
@@ -1,5 +1,9 @@
+try:
+   from KilluaRobot.modules.sql import BASE, SESSION
+except ImportError:
+    raise AttributeError
+
 from sqlalchemy import Column, String, Numeric, Boolean
-from KilluaRobot.modules.sql import BASE, SESSION
 
 
 class forceSubscribe(BASE):
